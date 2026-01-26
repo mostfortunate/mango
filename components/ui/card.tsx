5277fe6099c6bf560f52730e4d7a0263ddcd066b
Asaad Ahmed <asaadbinahmed@gmail.com>
2026-01-26 04:54:10 -0500

    adjust padding

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-2 shadow-sm",
+        "bg-card text-card-foreground flex flex-col gap-3 rounded-xl border py-3 shadow-sm",
         className
       )}
       {...props}
@@ -20,7 +20,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-header"
       className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-2 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-2",
+        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-3 px-3 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-3",
         className
       )}
       {...props}
@@ -53,7 +53,7 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-action"
       className={cn(
-        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
+        "col-start-3 row-span-3 row-start-1 self-start justify-self-end",
         className
       )}
       {...props}
@@ -65,7 +65,7 @@ function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-content"
-      className={cn("px-2", className)}
+      className={cn("px-3", className)}
       {...props}
     />
   )
@@ -75,7 +75,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-footer"
-      className={cn("flex items-center px-2 [.border-t]:pt-2", className)}
+      className={cn("flex items-center px-3 [.border-t]:pt-3", className)}
       {...props}
     />
   )
