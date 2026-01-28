ebc0ae0e7e46d0fca1768d263eb2c4cc2178f5ca
mostfortunate <asaadbinahmed@gmail.com>
2026-01-28 03:18:53 -0500

    refactor: extracted response tabs to ResponseTabs & integrated, then removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -5,42 +5,14 @@ import { status, type HttpStatus } from "http-status";
 import { json } from "@codemirror/lang-json";
 import { useTheme } from "next-themes";
 import axios, { type AxiosResponse } from "axios";
-import prettyBytes from "pretty-bytes";
 
 import { EditorView } from "@uiw/react-codemirror";
 import { githubLight, githubDark } from "@uiw/codemirror-theme-github";
-import CodeMirror from "@uiw/react-codemirror";
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Copy } from "lucide-react";
 
 import { HTTPMethod } from "@/app/types/http";
 import RequestForm from "@/components/RequestForm";
 import RequestTabs from "@/components/RequestTabs";
+import ResponseTabs from "@/components/ResponseTabs";
 
 type QueryParam = {
   key: string;
@@ -64,7 +36,6 @@ export default function Home() {
   const [headers, setHeaders] = useState<Header[]>([]);
   const [response, setResponse] = useState<AxiosResponse | null>(null);
   const methods: HTTPMethod[] = ["GET", "POST", "PUT", "DELETE", "PATCH"];
-  const responseTabs = ["Body", "Headers"];
 
   const { resolvedTheme } = useTheme();
   const noFocusOutlineRule = EditorView.theme({
@@ -202,82 +173,12 @@ export default function Home() {
           editorConfig={editorConfig}
         />
         {response && (
-          <Tabs defaultValue={responseTabs[0]} className="w-full">
-            <TabsList variant="line" className="mb-4 w-full flex items-center">
-              <div>
-                {responseTabs.map((tab) => (
-                  <TabsTrigger key={tab} value={tab}>
-                    {tab}
-                  </TabsTrigger>
-                ))}
-              </div>
-              <div className="flex gap-4 text-xs font-semibold ml-auto">
-                <span>
-                  {response.status} {getStatusText(response)}
-                </span>
-                <span>{response.customData.time} ms</span>
-                <span>
-                  {prettyBytes(
-                    JSON.stringify(response.data).length +
-                      JSON.stringify(response.headers).length,
-                  )}
-                </span>
-              </div>
-            </TabsList>
-            <TabsContent value="Body">
-              <Card>
-                <CardHeader className="flex flex-row justify-between">
-                  <div className="flex flex-col gap-3">
-                    <CardTitle>Response Body</CardTitle>
-                    <CardDescription>
-                      View the JSON response body from your request.
-                    </CardDescription>
-                  </div>
-                  <CardAction>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <Button
-                          variant="secondary"
-                          className="hover:bg-muted/90 hover:text-secondary transition-colors"
-                          size="icon-xs"
-                          aria-label="Copy response body"
-                        >
-                          <Copy />
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Copy</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </CardAction>
-                </CardHeader>
-                <CardContent className="flex flex-col gap-2">
-                  <CodeMirror
-                    value={responseBody}
-                    editable={false}
-                    {...editorConfig}
-                  />
-                </CardContent>
-              </Card>
-            </TabsContent>
-            <TabsContent value="Headers">
-              <Card>
-                <CardContent className="flex flex-col gap-2">
-                  {Object.entries(response.headers).map(
-                    ([key, value], index) => (
-                      <div
-                        key={index}
-                        className="flex flex-row items-center gap-2"
-                      >
-                        <Input readOnly value={key} />
-                        <Input readOnly value={value} />
-                      </div>
-                    ),
-                  )}
-                </CardContent>
-              </Card>
-            </TabsContent>
-          </Tabs>
+          <ResponseTabs
+            response={response}
+            responseBody={responseBody}
+            getStatusText={getStatusText}
+            editorSettings={editorConfig}
+          />
         )}
       </div>
     </>
