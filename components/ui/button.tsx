9fba3d02db85bd1e9470a4b3099dde2c5b87e666
mostfortunate <asaadbinahmed@gmail.com>
2026-01-29 00:41:19 -0500

    added button press effect

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -55,7 +55,7 @@ function Button({
       data-slot="button"
       data-variant={variant}
       data-size={size}
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, className }), "active:scale-95 duration-150")}
       {...props}
     />
   )
